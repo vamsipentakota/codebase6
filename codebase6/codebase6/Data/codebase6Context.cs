4f7bfeaf5610b93510907bac021b806e9a77e5f4
Vamsi Pentakota <v.pentakota@zensar.com>
2022-07-11 11:35:10 +0530

    codebaseexam6

---
+++ codebase6/codebase6/Data/codebase6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using codebase6.Model;
+
+namespace codebase6.Data
+{
+    public class codebase6Context : DbContext
+    {
+        public codebase6Context (DbContextOptions<codebase6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<codebase6.Model.Employee> Employee { get; set; }
+    }
+}
